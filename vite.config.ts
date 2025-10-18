55c26ea39582b1dfdeea8373fd4465bd23dd3c6c
Jomer Barcenilla <89145244+JomBarce@users.noreply.github.com>
2025-10-19 03:30:02 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const BACKEND_PORT = Number(process.env.PORT) || 9001;
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 9000,
     proxy: {
-      '/api': 'http://localhost:9001'
+      '/api': {
+        target: `http://localhost:${BACKEND_PORT}`,
+        changeOrigin: true,
+      },
     }
-  }
+  },
+  build: {  
+    target: 'esnext',
+    outDir: 'dist/',
+  },
 })
