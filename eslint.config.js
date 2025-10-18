d4ba937ae84f7277b572cb210e5d4da08f2993cf
Jomer Barcenilla <89145244+JomBarce@users.noreply.github.com>
2025-10-19 03:30:25 +0800

    Updated eslint.config.ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,36 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+import { defineFlatConfig, flatConfigsToRules } from 'eslint-define-config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default defineFlatConfig([
+  {
+    ignores: ['dist', 'node_modules'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
       globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      ...reactHooks.configs['recommended'].rules,
+      ...reactRefresh.configs.vite.rules,
     },
   },
 ])
