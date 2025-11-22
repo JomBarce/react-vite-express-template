8b0f39d5b3c08d35b918a63a29ffcb7b575f70db
JomBarce <89145244+JomBarce@users.noreply.github.com>
2025-11-23 01:32:24 +0800

    Updated server

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,10 +4,27 @@ dotenv.config();
 import { createServer } from 'http';
 import app from './app.js';
 
-const server = createServer(app);
+// Choose DB based on config
+import { connectMongo } from './db/mongo.js';
+import { connectMySQL } from './db/mysql.js';
 
 const PORT = Number(process.env.PORT) || 9001;
 
-server.listen(PORT, () => {
-  console.log(`API started on http://localhost:${PORT}`);
-});
+async function startServer() {
+  if (process.env.USE_MONGO === 'true') {
+    await connectMongo();
+  }
+  
+  if (process.env.USE_MYSQL === 'true') {
+    await connectMySQL();
+  }
+
+  const server = createServer(app);
+
+  server.listen(PORT, () => {
+    console.log(`API started on http://localhost:${PORT}`);
+  });
+
+}
+
+startServer();
