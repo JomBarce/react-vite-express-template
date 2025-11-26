a5c122befd2f1b458968cb623ed9971d5ed4679f
JomBarce <89145244+JomBarce@users.noreply.github.com>
2025-11-27 03:06:55 +0800

    Updated server

--- src/server/db/mysql.ts
+++ src/server/db/mysql.ts
@@ -1,18 +1,27 @@
 import mysql from "mysql2/promise";
 
-let pool: mysql.Pool;
+let pool: mysql.Pool | null = null;
 
 export async function connectMySQL() {
 	const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
-  if (!MYSQL_HOST || !MYSQL_USER || !MYSQL_DATABASE) throw new Error("Missing MySQL environment variables");
+    if (
+        MYSQL_HOST === undefined ||
+        MYSQL_USER === undefined ||
+        MYSQL_PASSWORD === undefined ||
+        MYSQL_DATABASE === undefined
+    ) {
+        throw new Error("Missing MySQL environment variables");
+    }
 
-	pool = mysql.createPool({
-		host: MYSQL_HOST,
-		user: MYSQL_USER,
-		password: MYSQL_PASSWORD,
-		database: MYSQL_DATABASE,
-		connectionLimit: 10,
-	});
+    if (!pool) {
+        pool = mysql.createPool({
+            host: MYSQL_HOST,
+            user: MYSQL_USER,
+            password: MYSQL_PASSWORD,
+            database: MYSQL_DATABASE,
+            connectionLimit: 10,
+        });
+    }
 
 	try {
 		const [rows] = await pool.query("SELECT 1");
